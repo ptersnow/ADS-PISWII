a96ade8f2c444d9a538fc417be5d3e0b2199a995
Pedro Henrique Neves da Silva <ptersnow@gmail.com>
2026-06-24 19:06:47 -0300

    Primeiro commit do projeto

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+// public/index.php
+
+require_once __DIR__ . '/../app/Controllers/HomeController.php';
+
+// Teste rápido para validar o ambiente e classes
+echo "<h1>Ambiente configurado com sucesso!</h1>";
